cd71f2674b9cb52ab7edd7b5e8dcf35a5c9ae551
Denis Talakevich <cnpaut@gmail.com>
2017-04-16 19:18:21 +0300

    some refactoring

---
+++ api/api.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"fmt"
+	"github.com/xanzy/go-gitlab"
+	"os"
+	"strings"
+)
+
+type Opts struct {
+	Token     *string
+	Project   *string
+	SrcBranch *string
+	DstBranch *string
+	Assignee  *string
+	Title     *string
+	BaseUrl   *string
+}
+
+func failErr(str string, err error) {
+	failStr(str + "\n" + err.Error())
+}
+
+func failStrf(format string, x ...interface{}) {
+	failStr(fmt.Sprintf(format, x...))
+}
+
+func failStr(str string) {
+	fmt.Println("Error: " + str)
+	os.Exit(1)
+}
+
+func CreateMergeRequest(opts *Opts) string {
+	cli := setupApi(*opts.BaseUrl, *opts.Token)
+	return createMergeRequest(cli, opts)
+}
+
+func findUser(api *gitlab.Client, username *string) *gitlab.User {
+	usersOpts := gitlab.ListUsersOptions{Username: username}
+	usersList, _, err := api.Users.ListUsers(&usersOpts)
+	if err != nil {
+		failErr("Can't fetch gitlab users", err)
+	}
+	if len(usersList) == 0 {
+		failStrf("Can't found assignee %#v", *username)
+	}
+	return usersList[0]
+}
+
+func setupApi(url string, token string) *gitlab.Client {
+	cli := gitlab.NewClient(nil, token)
+	if strings.HasSuffix(url, "/") {
+		url = strings.TrimSuffix(url, "/")
+	}
+	if !strings.HasSuffix(url, "/api/v3") {
+		url += "/api/v3"
+	}
+	cli.SetBaseURL(url)
+	return cli
+}
+
+func createMergeRequest(cli *gitlab.Client, opts *Opts) string {
+	assigneeID := findUser(cli, opts.Assignee).ID
+
+	mrOpts := gitlab.CreateMergeRequestOptions{
+		SourceBranch: opts.SrcBranch,
+		TargetBranch: opts.DstBranch,
+		Title:        opts.Title,
+		AssigneeID:   &assigneeID,
+	}
+	mr, _, err := cli.MergeRequests.CreateMergeRequest(*opts.Project, &mrOpts)
+	if err != nil {
+		failErr("Can't create merge request", err)
+	}
+	webUrl := strings.Replace(cli.BaseURL().String(), "/api/v3/", "", 1)
+	return fmt.Sprintf("%s/%s/merge_requests/%v", webUrl, *opts.Project, mr.IID)
+}
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"github.com/senid231/gitlab-cli/api"
+	"github.com/senid231/goopt"
+	"os"
+)
+
+func newOpts() *api.Opts {
+	argOpts := api.Opts{}
+	argOpts.Token = goopt.StringWithLabel([]string{"-A", "--access-token"}, "", "<token>", "private access token")
+	argOpts.Project = goopt.StringWithLabel([]string{"-P", "--project"}, "", "<name>", "namespaced name of project")
+	argOpts.SrcBranch = goopt.StringWithLabel([]string{"-s", "--src-branch"}, "", "<branch>", "namespaced name of project")
+	argOpts.DstBranch = goopt.StringWithLabel([]string{"-t", "--target-branch"}, "", "<branch>", "target branch")
+	argOpts.Assignee = goopt.StringWithLabel([]string{"-a", "--assignee"}, "", "<username>", "assignee username")
+	argOpts.Title = goopt.StringWithLabel([]string{"-T", "--title"}, "", "<text>", "title of MR")
+	argOpts.BaseUrl = goopt.StringWithLabel([]string{"-U", "--url"}, "https://gitlab.com", "<baseUrl>", "base URL for gitlab API")
+	return &argOpts
+}
+
+func parseArgs() []string {
+	goopt.Description = func() string {
+		return "Console gitlab client.\n" +
+			"Use v3 API"
+	}
+	goopt.Version = "1.0.0"
+	goopt.Summary = "api [options] <subcommand> <action> [options]"
+	goopt.Author = "Denis Talakevich <senid231@gmail.com>"
+	goopt.ExtraUsage = "Subcommands:\n" +
+		"  mr\t\tMerge Request manipulations\n" +
+		"  Actions:\n" +
+		"    create\tcreates merge request"
+
+	goopt.Parse(nil)
+	return goopt.Args
+}
+
+func optErr(str string) {
+	fmt.Println(goopt.Usage(), "\n"+str)
+	os.Exit(1)
+}
+
+func optErrf(format string, x ...interface{}) {
+	optErr(fmt.Sprintf(format, x...))
+}
+
+// gitlab-ci mr create -P namespace/project -s my-branch -t master -a senid231 -A "TOKEN" -T "some title" -U "https://gitlab.com/api/v3"
+
+func main() {
+	opts := newOpts()
+	args := parseArgs()
+	if len(args) != 2 {
+		optErr("invalid arguments")
+	}
+	switch {
+	case args[0] == "mr" && args[1] == "create":
+		fmt.Println(api.CreateMergeRequest(opts))
+	default:
+		optErrf("invalid subcommmand and/or action: %s %s", args[0], args[1])
+	}
+}
